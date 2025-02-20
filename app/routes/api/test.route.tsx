5dcf951d893115d274dec25d83fd6e1639ba2273
syn <54985049+Synitx@users.noreply.github.com>
2025-02-20 12:04:42 +0530

    Fixed rate limiting

--- app/routes/api/test.route.tsx
+++ app/routes/api/test.route.tsx
@@ -3,9 +3,10 @@ import RateLimiter from '~/lib/RateLimiter';
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const ip = request.headers.get('X-Forwarded-For');
+    console.log(ip);
     const limiter = RateLimiter()
     try {
-        await limiter(request, null, () => { });
+        await limiter(ip, null, () => { });
     } catch (error) {
         return new Response(JSON.stringify({ error: 'Too many requests' }), { status: 429 })
     }
