77f8abc4a0658d1738db02099796112c67c45a8c
Synitx <sxodeveloper@gmail.com>
2025-02-28 13:22:41 +0530

    meta update

--- app/routes/about._index.tsx
+++ app/routes/about._index.tsx
@@ -28,6 +28,18 @@ export const meta: MetaFunction = () => {
     return [
         { title: "About Us - Moon Software" },
         { name: "description", content: "Elevate your game with stunning assets, UI/UX, and environments from skilled developers." },
+        {
+            property: "og:image",
+            content: "/banner.webp",
+        },
+        {
+            property: "og:image:width",
+            content: "1280",
+        },
+        {
+            property: "og:image:height",
+            content: "720",
+        }
     ];
 };
 
