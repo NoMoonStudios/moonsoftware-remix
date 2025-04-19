5d1fcab31839545a951b9ec0284b77e1c87ea3cc
syllycato <sylly.cato@gmail.com>
2025-04-19 18:19:55 +0100

    Updated Settings

--- app/routes/settings._index.tsx
+++ app/routes/settings._index.tsx
@@ -1,9 +1,8 @@
 import { SettingsIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import { useEffect, useState } from "react";
-import { CgPaypal } from "react-icons/cg";
+import { CgSpinner } from "react-icons/cg";
 import Navigation from "~/components/pages/Navigation"
-import Portfolio from "~/components/pages/Settings/Portfolio";
 import Profile from "~/components/pages/Settings/Profile";
 import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
@@ -14,31 +13,26 @@ import { UserInfo } from "~/types/init";
 const Settings = () => {
   const [userInfo, setUserInfo] = useState<UserInfo|undefined>();
   const [selected, setSelected] = useState("Profile")
+  const [loading, setLoading] = useState(false)
 
   async function LoadUser(){
+    setLoading(true);
     const data = await GetUserSession()
     if (!data) return;
     
     setUserInfo(data);
+    setLoading(false);
   }
   
   const buttonStyle = "h-12 cursor-pointer duration-200 rounded-none border-l-4 hover:border-white hover:bg-transparent"
   
   const items = userInfo ? {
     Profile: <Profile userInfo={userInfo} onUpdate={LoadUser}/>,
-    Portfolio: <Portfolio userInfo={userInfo}/>,
-    Account: <h1 className="text-2xl font-bold">wow didnt do</h1>,
-    Commisions: <>or this</>,
-    Billing: <>im too lazy</>,
-    Privacy: <div className="flex flex-col h-full">
-      anyways total cost will be:
-      <div className="text-green-500 text-2xl">$4,499.99 <span className="text-white text-sm opacity-35">Taxes not included</span></div>
-      <Separator className="w-full mt-4 mb-8"/>
-      <Button className="cursor-pointer w-min"><CgPaypal/> Pay Via PayPal</Button>
-      <p className="text-red-700 text-sm mt-4">(failure to pay will result in heavy consequences)</p>
-      <div className="grow"></div>
-      <Button variant={"outline"} className="cursor-pointer mt-auto w-min">Commit Suicide</Button>
-    </div>,
+    Portfolio: <div className="flex justify-center items-center w-full h-full text-3xl text-gray-600">Coming Soon</div>,
+    Account: <div className="flex justify-center items-center w-full h-full text-3xl text-gray-600">Coming Soon</div>,
+    Commisions: <div className="flex justify-center items-center w-full h-full text-3xl text-gray-600">Coming Soon</div>,
+    Billing: <div className="flex justify-center items-center w-full h-full text-3xl text-gray-600">Coming Soon</div>,
+    Privacy: <div className="flex justify-center items-center w-full h-full text-3xl text-gray-600">Coming Soon</div>,
   } : {}
   
   useEffect(() => {
@@ -48,10 +42,10 @@ const Settings = () => {
 
 
   return (
-    <div className="w-full h-full flex flex-col">
+    <div className="w-full h-screen flex flex-col">
       <Navigation userInfo={userInfo}/>
       <div className="w-full flex grow justify-center mt-10 mb-10">
-        <div className="w-7xl flex flex-row min-h-80">
+        <div className="w-full max-w-[1600px] flex flex-row min-h-80">
           <div className="w-[250px] flex flex-col pr-5 pl-5">
             <div className="w-full h-12 flex flex-row justify-center items-center gap-2 mb-3 mt-3 text-xl">
               <SettingsIcon/> Settings
@@ -77,11 +71,12 @@ const Settings = () => {
                     {selected}
                   </motion.h1> 
                 }
+
+                {loading && <CgSpinner className="animate-spin ml-auto" size={25}/>}
               </div>
               <Separator key="main-separator" className="w-full"/>
               { userInfo && selected &&
-                <div key="content-container" className="w-full grow flex relative">
-
+                <div key="content-container" className="w-full grow flex relative overflow-y-auto">
                 <motion.div 
                   key={selected} 
                   initial={{opacity: 0}}
